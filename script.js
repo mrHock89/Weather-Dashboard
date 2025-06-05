55a798e1cc21a9ba72cf54a37654377c006a5cd2
mrHock89 <147297458+mrHock89@users.noreply.github.com>
2025-06-05 09:29:31 +0530

    implement a function to get weatherinfo

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const myapiKey = '4283fea570f1791a0da8ec708eaabf60';
 
 let serachBtn = document.getElementById('searchBtn');
 let cityInput = document.getElementById('cityInput');
+let weatherInfo = document.getElementById('weatherInfo');
 
 serachBtn.addEventListener('click',() => {
     const city = cityInput.ariaValueMax.trim();
@@ -12,3 +13,24 @@ serachBtn.addEventListener('click',() => {
         alert('Enter a city name or zip code.');
     }
 });
+
+// For getting the weather
+
+async function getWeather(city){
+    try {
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+        if(!res.ok) throw new Error('City is not found.');
+        const data = await res.json();
+
+        weatherInfo.innerHTML = `
+        <h2>${data.name}</h2>
+        <p>Temperature: ${data.main.temp}<p>
+        <p>Wind Speed: ${data.wind.speed} m/s</p>
+        <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Weather Icon">
+        `;
+    } catch (error) {
+        weatherInfo.innerHTML = `
+        <p>${error.message}</p>
+        `;
+    }
+}
